ede52e9fd29f53f497db26e73a6b5434f5b07b69
John Michadasis <johnmichadasis@gmail.com>
2025-11-18 23:56:42 +0200

    Modify copyright notice in BAC-calc.py
    Updated copyright information to include Ioannis Michadasis.

--- src/BAC-calc.py
+++ src/BAC-calc.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) 2025 Apostolos Chalis
+Copyright (c) 2025 Apostolos Chalis, Ioannis Michadasis
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
