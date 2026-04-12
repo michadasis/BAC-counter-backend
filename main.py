9ec8aed270144de190e7f0889b17250bdd44a08b
michadasis <johnmichadasis@gmail.com>
2026-04-12 15:53:40 +0300

    Removed Temp fix

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        #"http://localhost:3000", 
-        #"https://alccalc.vercel.app",
-        "*",
+        "http://localhost:3000", 
+        "https://alccalc.vercel.app",
+        #"*",
         ],
     allow_credentials=True,
     allow_methods=["*"],
